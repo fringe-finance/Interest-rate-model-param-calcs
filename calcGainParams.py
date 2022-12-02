a3c504cdbb49b4f8050f9b7a062b81b07182ee6c
Dist bit <distbit@pm.me>
2022-12-02 13:31:39 +1100

    first commit

---
+++ calcGainParams.py
@@ -0,0 +1,37 @@
+timePeriodDays = 1 / 48
+blocksPerYear = 2102400  # correct value: 2628000, incorrect value: 2102400
+maxIncreasePerPeriod = 0.05
+maxDecreasePerPeriod = 0.05
+targetUtilisationRate = 0.5
+
+
+#######################      GAIN      #######################
+gain = (maxDecreasePerPeriod * (365 / timePeriodDays) * (10**18)) / (
+    blocksPerYear * targetUtilisationRate
+)
+HRgain = (maxDecreasePerPeriod * (365 / timePeriodDays)) / (targetUtilisationRate)
+
+
+#######################      JUMP GAIN      #######################
+jumpGain = (
+    (maxIncreasePerPeriod / maxDecreasePerPeriod) * targetUtilisationRate * (10**18)
+) / (1 - targetUtilisationRate)
+HRjumpGain = ((maxIncreasePerPeriod / maxDecreasePerPeriod) * targetUtilisationRate) / (
+    1 - targetUtilisationRate
+)
+
+
+#######################      TARGET UTILISATION      #######################
+targetUtil = targetUtilisationRate * (10**18)
+HRtargetUtil = targetUtilisationRate
+
+
+print("\nFOR SMART CONTRACT:")
+print("Gain:", round(gain, 1))
+print("Jump gain:", round(jumpGain, 1))
+print("Target utilisation rate:", round(targetUtil, 1))
+
+print("\n\n\nFOR HUMANS:")
+print("Gain:", round(HRgain, 3))
+print("Jump gain:", round(HRjumpGain, 3))
+print("Target utilisation rate:", round(HRtargetUtil, 3))
