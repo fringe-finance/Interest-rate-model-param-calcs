6b3878defb19f4fd5ae224be4bd6da0561abdc0c
Dist Bit <adp01@pm.me>
2023-11-30 17:17:41 +1100

    getInterestRate.py

--- getInterestRate.py
+++ getInterestRate.py
@@ -1,11 +1,23 @@
+import argparse
 from web3 import Web3
 
-# Initialize web3
-w3 = Web3(
-    Web3.HTTPProvider(
-        "https://eth-goerli.g.alchemy.com/v2/EkJ6KVy7Q1Cu77daPHQo8VVn20m55pzJ"
-    )
+# Create the argument parser
+parser = argparse.ArgumentParser(description="Script to calculate APYs")
+
+# Define arguments
+parser.add_argument("--rpc", required=True, help="The RPC URL")
+parser.add_argument("--blend", required=True, help="Blending token contract address")
+parser.add_argument(
+    "--jumpRate", required=True, help="Jump rate model contract address"
 )
+parser.add_argument(
+    "--blocksPerYear", type=int, required=True, help="Number of blocks per year"
+)
+
+# Parse the arguments
+args = parser.parse_args()
+# Initialize web3
+w3 = Web3(Web3.HTTPProvider(args.rpc))
 
 # ABI for BlendingTokenProxy
 blendingTokenProxy_abi = """
@@ -25,12 +37,12 @@ jumpRateModelV3Proxy_abi = """
 ]
 """
 
-# Constants and addresses
-blendingTokenProxy_address = "0xcB034b9A387DA193F524aB9E222f909dfEDC08c9"
-jumpRateModelV3Proxy_address = "0xDaad874Ec0dd2345F1Ec05959CFfBb7906fB4F9d"
-blocksPerYear = 2628000
-blocksPerDay = blocksPerYear / 365
+blendingTokenProxy_address = args.blend
+jumpRateModelV3Proxy_address = args.jumpRate
+blocksPerYear = args.blocksPerYear
 daysPerYear = 365
+blocksPerDay = blocksPerYear / daysPerYear
+
 
 # Initialize contract instances
 blendingTokenProxy_contract = w3.eth.contract(
@@ -71,3 +83,4 @@ print(f"Reserve Factor: {int(reserveFactorMantissa/1e18*100*10)/10}%")
 print(f"Utilization Rate: {int(utilization_rate*1000)/10}%")
 print(f"Borrow APY: {int(borrowAPY * 10) / 10}%")
 print(f"Supply APY: {int(supplyAPY * 10) / 10}%")
+print(f"RAW New Borrow Rate: {borrowAPY}%")
